30dc3209c713bdf70664235ec8b15fc85626ae2a
Hiesenberg <menanafen@gmail.com>
2019-01-28 01:28:39 +0200

    went to main js

--- js/myFunction.js
+++
@@ -1,9 +0,0 @@
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("stick");
-    }
-    else {
-        header.classList.remove("stick");
-    }
-}
